a447d3d0b60f05d18cd48727c45f8c73ca94941a
David Kurth <david-kurth@sbcglobal.net>
2016-12-05 22:36:17 -0600

    update

--- week-6/day-26/day-26-multi-step-form/multi-form.js
+++ week-6/day-26/day-26-multi-step-form/multi-form.js
@@ -1,26 +1,7 @@
 
-// var clickNext = document.querySelector(".next-button");
-//
-// clickNext.addEventListener('click', function(evt) {
-//   if (evt.target.classList == 'second-page')
-// });
-//
-// // note
-//
-// var clickNext =
-// document.querySelector(".start-page").classList.add("name-form");
-//
-// clickNext.addEventListener('click', function(evt) {
-//
-// })
+ var start = document.querySelector(".next-button");
 
- var start = document.querySelector(".start-page");
-
- start.addEventListener('click', function(evt) {
-   if (evt.target.classList == 'next-button') {
-  remove
-   }
-   else {
-  add
-   }
- });
+start.addEventListener('click', function(){
+  var show = document.querySelector('.age-form');
+  show.classList.add('on')
+});
