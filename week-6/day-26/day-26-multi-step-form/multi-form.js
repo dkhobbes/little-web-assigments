cbd1cd8b47b03d0a1e3f3a0ecec1dcb1f8f567e5
David Kurth <david-kurth@sbcglobal.net>
2016-12-05 22:05:36 -0600

    working on add and removing in the form still

--- week-6/day-26/day-26-multi-step-form/multi-form.js
+++ week-6/day-26/day-26-multi-step-form/multi-form.js
@@ -1,6 +1,26 @@
 
-var clickNext = document.querySelector(".next-button");
+// var clickNext = document.querySelector(".next-button");
+//
+// clickNext.addEventListener('click', function(evt) {
+//   if (evt.target.classList == 'second-page')
+// });
+//
+// // note
+//
+// var clickNext =
+// document.querySelector(".start-page").classList.add("name-form");
+//
+// clickNext.addEventListener('click', function(evt) {
+//
+// })
 
-clickNext.addEventListener('click', function(evt) {
-  if (evt.target.classList == 'second-page')
-});
+ var start = document.querySelector(".start-page");
+
+ start.addEventListener('click', function(evt) {
+   if (evt.target.classList == 'next-button') {
+  remove
+   }
+   else {
+  add
+   }
+ });
