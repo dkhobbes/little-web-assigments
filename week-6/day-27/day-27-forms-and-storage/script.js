aa66885ffa19dce5b432eb69cd3b3c62ef26075d
David Kurth <david-kurth@sbcglobal.net>
2016-12-06 11:05:28 -0600

    adding updates to forms and storage

--- week-6/day-27/day-27-forms-and-storage/script.js
+++ week-6/day-27/day-27-forms-and-storage/script.js
@@ -1,16 +1,32 @@
 
 (function () {
+
   var button = document.querySelector('.add-person-button');
+  var firstNameInput = document.querySelector('.first-name-input')
+  var LastNameInput = document.querySelector('.last-name-input')
+
+
   var peopleList = document.querySelector('.people-list');
-  button.addEventListener('click', function () {
+  button.addEventListener('click', addPerson);
 
-    var firstName = 'Bob';
-    var lastName = 'Smith';
-
+  function addPerson() {
+    var firstName = firstNameInput.value;
+    var lastName = LastNameInput.value;
     var li = document.createElement('li');
+    li.textContent = firstName + ' ' + lastName;
     peopleList.appendChild(li);
+    firstNameInput.value = '' ;
+    LastNameInput.value = '' ;
+  }
 
+
+  LastNameInput.addEventListener('keyup', function(evt) {
+    console.log('keyup', LastNameInput.value, evt);
+    if (evt.keyCode === 13) {
+      addPerson();
+    }
   });
+
 
 
 }) ();
