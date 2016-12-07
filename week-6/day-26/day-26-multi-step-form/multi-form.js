a5f2ad5b98d0e05e0555ab55c1fbd1086b8c2caf
David Kurth <david-kurth@sbcglobal.net>
2016-12-06 21:14:42 -0600

    add multi step 1

--- week-6/day-26/day-26-multi-step-form/multi-form.js
+++ week-6/day-26/day-26-multi-step-form/multi-form.js
@@ -1,4 +1,48 @@
 
+(function() {
+
+  var position = 1;
+  var nextButton = document.querySelector('.next-button');
+  var previousButton = document.querySelector('.previous-button');
+
+  nextButton.addEventListener('click',function() {
+      if (position < 4){
+
+      position++;
+      showBasedOnCurrentPosition();
+      }
+    });
+
+  previousButton.addEventListener('click',function() {
+    if (position > 1) {
+      position--;
+      showBasedOnCurrentPosition();
+    }
+  });
+
+  function showBasedOnCurrentPosition() {
+
+      var currentlyShowing = document.querySelector('.on');
+      currentlyShowing.classList.remove('on');
+
+      var selector = '.form-' + position;
+      console.log('selector', selector);
+
+      var thing = document.querySelector(selector);
+      thing.classList.add('on');
+
+    }
+
+})();
+
+
+
+// note for now
+
+
+
+
+
  var start = document.querySelector(".next-button");
 
 start.addEventListener('click', function(){
