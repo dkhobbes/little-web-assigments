392a03e297aa2e02bc29c5e39ce6a9352d604067
David Kurth <david-kurth@sbcglobal.net>
2016-12-07 11:08:28 -0600

    adding updates

--- week-6/day-28/script.js
+++ week-6/day-28/script.js
@@ -7,21 +7,57 @@ submitButton.addEventListener('click', function(evt) {
   var firstNameInput = document.querySelector('.first-name');
   var lastNameInput = document.querySelector('.last-name');
 
-  if (firstNameInput.value === '') {
-    evt.preventDefault();
-    firstNameInput.classList.add('error');
-  }
-  else {
-    firstNameInput.classList.remove('error');
-  }
-  if (lastNameInput.value === '') {
-    evt.preventDefault();
-    lastNameInput.classList.add('error');
-  }
-  else {
-    lastNameInput.classList.remove('error');
-  }
+  // if (firstNameInput.value === '') {
+  //   evt.preventDefault();
+  //   firstNameInput.classList.add('error');
+  // }
+  // else {
+  //   firstNameInput.classList.remove('error');
+  // }
+  // if (lastNameInput.value === '') {
+  //   evt.preventDefault();
+  //   lastNameInput.classList.add('error');
+  // }
+  // else {
+  //   lastNameInput.classList.remove('error');
+  // }
+
+  var bioText = document.querySelector('.bio');
+
+  // if (bioText.value.length < 20) {
+  //   evt.preventDefault();
+  //   bioText.classList.add('error');
+  // }
+  // else {
+  //   bioText.classList.remove('error');
+  // }
+
+  validateRequired(evt, firstNameInput);
+  validateRequired(evt, lastNameInput);
+  validateLength(evt, bioText);
+
 });
+
+function validateRequired(evt, theDomElement) {
+  if (theDomElement.value === '') {
+    evt.preventDefault();
+    theDomElement.classList.add('error');
+  }
+  else {
+    theDomElement.classList.remove('error');
+  }
+}
+
+function validateLength(evt, theDomElement) {
+  if (theDomElement.value.length < 20 ) {
+    evt.preventDefault();
+    theDomElement.classList.add('error');
+  }
+  else {
+    theDomElement.classList.remove('error');
+  }
+}
+
 
 var googleLink = document.querySelector('.google-link');
 googleLink.addEventListener('click', function(evt) {
