d03954b9af9661eea1ba774359281a3bb1b7a80a
David Kurth <david-kurth@sbcglobal.net>
2016-12-08 09:55:07 -0600

    updating with function change

--- week-6/day-29/script.js
+++ week-6/day-29/script.js
@@ -1,17 +1,19 @@
 console.log('script file hooked up!');
 
 var clickMeButton =document.querySelector('.click-me');
-
 var clickMeTooButton = document.querySelector('.click-me-too');
+var thursdayDiv = document.querySelector('.thursday-div');
+
 
 clickMeButton.addEventListener('click', function() {
-  var thursdayDiv = document.querySelector('.thursday-div');
-  thursdayDiv.textContent = "different now";
-  thursdayDiv.classList.toggle('on');
+  changeIt("different one");
 });
 
 clickMeTooButton.addEventListener('click', function() {
-  var thursdayDiv = document.querySelector('.thursday-div');
-  thursdayDiv.textContent = "different now";
-  thursdayDiv.classList.toggle('on');
+  changeIt('another option');
 });
+
+function changeIt(message) {
+  thursdayDiv.textContent = message;
+  thursdayDiv.classList.toggle('on');
+}
