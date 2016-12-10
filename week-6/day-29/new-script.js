2cbe6a77ec40fc6e4ee086020683282109be858f
David Kurth <david-kurth@sbcglobal.net>
2016-12-10 15:45:10 -0600

    practice

--- week-6/day-29/new-script.js
+++ week-6/day-29/new-script.js
@@ -1,1 +1,10 @@
 var pizzas = ['Cheese', 'Pepperoni', 'Canadian Bacon', 'Mushrooms (gross)'];
+
+for (var i = 0; i < pizzas.length; i++) {
+  var pizza = pizzas[i];
+  console.log(pizza);
+}
+
+if (pizzas[i] == 'Canadian Bacon') {
+  console.log('this is the best pizza');
+}
