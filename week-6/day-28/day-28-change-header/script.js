fbe731fa3ee307e5507c3567d57708c1c3cd9a13
David Kurth <david-kurth@sbcglobal.net>
2016-12-11 21:50:56 -0600

    finished the header scroll and pushed the text down

--- week-6/day-28/day-28-change-header/script.js
+++ week-6/day-28/day-28-change-header/script.js
@@ -1,13 +1,14 @@
-console.log('tessting script')
 
 window.addEventListener('scroll', function(evt) {
 
-  if (window.pageYOffset > 225) {
-    console.log('too low!');
+  if (window.pageYOffset > 100) {
+    var headerSmall = document.querySelector('.heading-set');
+    headerSmall.classList.add('header-change');
+  }
+  if (window.pageYOffset < 99) {
+    var headerTall = document.querySelector('.heading-set');
+    headerTall.classList.remove('header-change');
   }
 
-});
 
-window.addEventListener('resize', function() {
-  console.log('ael width changed');
 });
