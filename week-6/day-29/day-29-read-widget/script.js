a9df12063d19b7c59364ae1b3271b3f849637e0e
David Kurth <david-kurth@sbcglobal.net>
2016-12-11 22:50:28 -0600

    updating current status on read widget

--- week-6/day-29/day-29-read-widget/script.js
+++ week-6/day-29/day-29-read-widget/script.js
@@ -1,1 +1,16 @@
 console.log('testing script');
+
+var divP =document.querySelectorAll("p");
+var input = document.querySelector('.input');
+
+
+var sum = 0;
+
+for (var i = 0; i < div.length; i++) {
+  var arr = div[i].textContent.split(" ");
+  sum = sum + arr.length;
+}
+
+var result = math.floor(sum/230);
+input.textContent = " " + result + " "
+console.log(result);
