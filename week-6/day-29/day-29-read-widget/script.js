ad243aa51cb69e22470c48445d6e064977a2d5af
David Kurth <david-kurth@sbcglobal.net>
2016-12-11 23:08:20 -0600

    completed the reading time widget

--- week-6/day-29/day-29-read-widget/script.js
+++ week-6/day-29/day-29-read-widget/script.js
@@ -6,11 +6,11 @@ var input = document.querySelector('.input');
 
 var sum = 0;
 
-for (var i = 0; i < div.length; i++) {
-  var arr = div[i].textContent.split(" ");
+for (var i = 0; i < divP.length; i++) {
+  var arr = divP[i].textContent.split(" ");
   sum = sum + arr.length;
 }
 
-var result = math.floor(sum/230);
-input.textContent = " " + result + " "
+var result = Math.floor(sum/230)
+input.textContent = "reading time: " + result + " minutes";
 console.log(result);
