be6fb44a7566986763d3e6a910e8792cfd342d89
David Kurth <david-kurth@sbcglobal.net>
2016-12-12 11:40:11 -0600

    adding new save point

--- week-7/day-31/day-31-github-users-part-deux/script.js
+++ week-7/day-31/day-31-github-users-part-deux/script.js
@@ -1,7 +1,24 @@
 (function () {
 
-  $.ajax({
-    url:''
+  var theList = document.querySelector('.user-list');
+
+  var promise = $.ajax({
+    url:'https://api.github.com/search/users?q=cheese'
   });
-  
+
+  promise.done(function(data) {
+    console.log('data is back', data);
+
+    for (var i = 0; i < data.items.length; i++) {
+      var user = data.items[i];
+      console.log('the user', user);
+
+      var li = document.createElement('li');
+      li.textContent = user.login;
+
+      theList.appendChild(li);
+    }
+
+  });
+
 })();
