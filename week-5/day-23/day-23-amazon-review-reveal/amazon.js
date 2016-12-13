c59563f5e54de44cfaf3cd7e7b4ccf80833c6525
David Kurth <david-kurth@sbcglobal.net>
2016-12-13 08:18:10 -0600

    new save point

--- week-5/day-23/day-23-amazon-review-reveal/amazon.js
+++ week-5/day-23/day-23-amazon-review-reveal/amazon.js
@@ -1,70 +1,34 @@
 var clickMe = document.querySelector('.read-style');
 
-  var readThis = document.querySelector('.main-text')
-  var  container = document.querySelector('.container')
-  var  readMore = document.querySelector('.')
-  var  readLess = document.querySelector('.')
-  var  readStyle = document.querySelector('.')
-
-        var height = constrainedHeight.clientHeight;
-
-        var heightNormal = containerHeight.clientHeight;
-
-        container.style.maxHeight = constrainedHeight.clientHeight + "px";
-
-        if (container.className === "container"){
-          container.style.maxHeight = heightNormal + "px";
-        } else if(container.className === "container show-container"){
-          container.style.maxHeight = height + "px";
-        };
-
-        container.classList.toggle("show-container");
-
-        setTimeout(function(){
-          readMore.classList.toggle("read-more");
-        }, 500); 
-        setTimeout(function(){
-          readLess.classList.toggle("read-more");
-        }, 500);
-
-      })
-
-});
-
-
-var trigger = document.querySelector(".read-more-trigger")
-  var container = document.querySelector(".container");
-  var readMore = document.querySelector(".read-more");
-  var readLess = document.querySelector(".read-less");
-  var arrow = document.querySelector(".arrow");
-  var containerHeight = document.querySelector(".client-height");
-  var constrainedHeight = document.querySelector(".shown")
-
-
+  var readThis = document.querySelector('.main-text');
+  var  container = document.querySelector('.container');
+  var  readMore = document.querySelector('.read-more');
+  var  readLess = document.querySelector('.read-less');
+  var  readStyle = document.querySelector('.');
 
   var height = constrainedHeight.clientHeight;
-
 
   var heightNormal = containerHeight.clientHeight;
 
   container.style.maxHeight = constrainedHeight.clientHeight + "px";
 
-  trigger.addEventListener("click", function(){
+  if (container.className === "container"){
+    container.style.maxHeight = heightNormal + "px";
+  }
+  else if(container.className === "container show-container"){
+    container.style.maxHeight = height + "px";
+  };
 
-    if (container.className === "container"){
-      container.style.maxHeight = heightNormal + "px";
-    } else if(container.className === "container show-container"){
-      container.style.maxHeight = height + "px";
-    };
+  container.classList.toggle("show-container");
 
-    container.classList.toggle("show-container");
+  setTimeout(function(){
+    readMore.classList.toggle("read-more");
+  }, 500);
 
-    setTimeout(function(){
-      readMore.classList.toggle("read-more");
-    }, 500);
-    setTimeout(function(){
-      readLess.classList.toggle("read-more");
-    }, 500);
+  setTimeout(function(){
+    readLess.classList.toggle("read-more");
+  }, 500);
 
-    arrow.classList.toggle("arrow-rotate");
-  })
+})
+
+});
