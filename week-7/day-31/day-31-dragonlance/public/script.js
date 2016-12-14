4c8bd077aac723bfe5695903f3cbacca341f9b7d
David Kurth <david-kurth@sbcglobal.net>
2016-12-14 10:33:45 -0600

    mustache add

--- week-7/day-31/day-31-dragonlance/public/script.js
+++ week-7/day-31/day-31-dragonlance/public/script.js
@@ -9,27 +9,35 @@ console.log('write code here!');
   promise.done(function(data) {
     console.log('data is back', data);
 
+    var html = '';
+
+
     for (var i = 0; i <data.characters.length; i++) {
-      var typeOf = data.characters[i];
-      console.log('the creature', typeOf);
-      var li = document.createElement('li');
-      li.textContent = typeOf.name;
-
-      theList.appendChild(li);
-
-      var raceP = document.createElement('p');
-      raceP.textContent = typeOf.race;
-      raceP.classList.add ('raceP');
-
-      li.appendChild(raceP);
-
-      var descriptionP = document.createElement('p');
-      descriptionP.textContent = typeOf.description;
-
-      li.appendChild(descriptionP);
+      var typeOfThing = data.characters[i];
 
 
+      // console.log('the creature', typeOf);
+      // var li = document.createElement('li');
+      // li.textContent = typeOf.name;
+      //
+      // theList.appendChild(li);
+      //
+      // var raceP = document.createElement('p');
+      // raceP.textContent = typeOf.race;
+      // raceP.classList.add ('raceP');
+      //
+      // li.appendChild(raceP);
+      //
+      // var descriptionP = document.createElement('p');
+      // descriptionP.textContent = typeOf.description;
+      //
+      // li.appendChild(descriptionP);
+
+      var output = Mustache.render('<li><p class="name">{{name}}</p><p class="raceP">{{race}}</p><p class="descriptionP">{{description}}</p></li>', typeOfThing);
+      console.log(output);
+      html += output;
     }
+    theList.innerHTML = html;
   });
 
 })();
