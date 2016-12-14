7d8f9b483900f36937d0934b16c027bf09a82365
David Kurth <david-kurth@sbcglobal.net>
2016-12-14 15:00:22 -0600

    new save point for beer

--- day-33-punkapi/script.js
+++ day-33-punkapi/script.js
@@ -1,5 +1,28 @@
 console.log("hi");
 
+console.log('look at this', btoa('hello'));
+console.log(atob('aGVsbG8='));
 var promise = $.ajax({
-  url:'https://punkapi.com/api/v1/beers'
+  url:'https://punkapi.com/api/v1/beers',
+  headers: {
+    "Authorization": "Basic " + btoa('6a856ff113074b919ac60d98074beaac:')
+
+},
+});
+
+promise.done(function(data) {
+  console.log('this data', data);
+
+  var beerStuffList= document.querySelector('.beer-stuff');
+  var templateScript = document.querySelector('.beer-template');
+  var templateHtml = templateScript.innerHTML;
+
+  var html = '';
+  for (var i = 0; i < data.length; i++) {
+    var beer = data[i];
+console.log(beer);
+    var output = Mustache.render(templateHtml, beer);
+    html += output;
+  }
+  beerStuffList.innerHTML = html;
 });
