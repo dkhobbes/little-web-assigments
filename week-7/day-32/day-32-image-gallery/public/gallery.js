f0570f2d0152deac09177ca587632e8f9ebe1d0e
David Kurth <david-kurth@sbcglobal.net>
2016-12-16 07:15:50 -0600

    finished img gallery

--- week-7/day-32/day-32-image-gallery/public/gallery.js
+++ week-7/day-32/day-32-image-gallery/public/gallery.js
@@ -2,19 +2,18 @@ var promise =$.ajax({
   url:'/api/pics'
 });
 
-var apiData;
+var apiPics;
 var thumbnailList = document.querySelector('.picture-thumbnail');
+var display = document.querySelector('.display');
 
 promise.done(function(data) {
-  apiData = data;
-  console.log('the data', data);
+  apiPics = data;
 
   for (var i = 0; i < data.pictures.length; i++) {
     var pic = data.pictures[i];
-    console.log('pic', pic);
-
     var imgLi = document.createElement('img');
     imgLi.src = pic.thumbnail;
+    imgLi.setAttribute('data-index',i)
     thumbnailList.appendChild(imgLi);
   }
 });
@@ -22,26 +21,21 @@ promise.done(function(data) {
 thumbnailList.addEventListener('click', function(evt) {
 console.log(evt.target);
 var attr = evt.target.getAttribute('data-index');
-console.log(attr);
 
-var currentlySelected = document.querySelector('.selected');
-  if (currentlySelected != null) {
-    currentlySelected.classList.remove('selected');
-  }
-    evt.target.classList.add('selected');
+// var currentlySelected = document.querySelector('.selected');
+//   if (currentlySelected != null) {
+//     currentlySelected.classList.remove('selected');
+//   }
+//     evt.target.classList.add('selected');
 
-var thumbImage = apiData.pictures[attr];
-console.log('the mutant', mutant);
+var thumbImage = apiPics.pictures[attr];
 
-var display = document.querySelector('.display');
 display.innerHTML = '';
-
-// starting from here
 
 var fullSizeImg = document.createElement('img');
 fullSizeImg.src = thumbImage.fullSize;
 
-fullSizeImg.appendChild(img);
+display.appendChild(fullSizeImg);
 
 
 var description = document.createElement('p');
@@ -49,6 +43,5 @@ description.textContent = thumbImage.description;
 
 display.appendChild(description);
 
-// check to see if it works from here
 
 });
