10a42ee65c5cb09efe5ebf897c668ebb7b249993
David Kurth <david-kurth@sbcglobal.net>
2016-12-16 16:30:16 -0600

    finished require data next step show data

--- week-7/day-34/day-34-github-repo-search/script.js
+++ week-7/day-34/day-34-github-repo-search/script.js
@@ -1,15 +1,29 @@
 (function(){
 
-  function requestData() {}
-  var promise = $.ajax({
-    url:'https://api.github.com/search'
+  var inputer =document.querySelector('.word-input');
+  var display =document.querySelector('.link-template');
+
+  function requestData() {
+    var promise = $.ajax({
+      url:'https://api.github.com/search/repositories?q=' + inputer.value
+    });
+
+    promise.done(function(data){
+    console.log('this data', data);
+    // for (var i= 0; i < data.length; i++) {
+    //   var item = data[i];
+    // }
+    });
+  }
+
+
+  inputer.addEventListener('keyup', function(evt) {
+    console.log(evt.keyCode);
+
+    if (evt.keyCode === 13) {
+      console.log('do something');
+      requestData()
+    }
   });
 
-promise.done(function(data){
-  console.log('this data', data);
-
-});
-
-}
-requestData();
 })();
