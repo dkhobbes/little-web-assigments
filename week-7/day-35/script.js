13e0fc434f3f9fa06d584e425a66188f142983f1
David Kurth <david-kurth@sbcglobal.net>
2016-12-16 13:40:33 -0600

    current progress

--- week-7/day-35/script.js
+++ week-7/day-35/script.js
@@ -1,23 +1,68 @@
-alert('hello');
+var gifList = document.querySelector('#gif-list');
+var nextButton = document.querySelector('.next');
+var previousButton = document.querySelector('.previous');
+var counter = document.querySelector('.counter');
+var page = 0;
+var displayPage = page + 1;
 
-var gifList = document.querySelector('#gif-list')
+nextButton.addEventListener('click', function() {
+  page += 1;
+  console.log('page is now', displayPage);
 
-$.ajax({
-  url:
-  'http://api.giphy.com/v1/gifs/search?q=megatron&api_key=dc6zaTOxFJmzC&offset=0'
-})
-.done(function(apiResult) {
-  console.log(apiResult);
+  localStorage.setItem('page-number', page);
 
-  for (var i = 0; i < apiResult.data.length; i++) {
-    var obj = apiResult.data[i];
-    console.log(obj);
-
-    var li = document.createElement('li');
-    var img = document.createElement('img');
-    li.appendChild(img);
-
-    gifList.appendChild(li);
-  }
+  loadData();
+  counter.innerHTML = displayPage;
 
 });
+
+previousButton.addEventListener('click', function() {
+  console.log('page is now at the beginning', displayPage);
+
+  if (page === 0) {
+    return;
+  }
+
+  page -=1;
+  localStorage.setItem('page-number', page);
+
+  loadData();
+  counter.innerHTML = displayPage;
+
+});
+
+
+counter.innerHTML = displayPage;
+
+console.log('page', page, 'storedValue', storedValue);
+
+var storedValue = storedValue.getItem('page-number');
+  if (storedValue !== null) {
+    page = Number(storedValue);
+  }
+
+function loadData(){
+
+  $.ajax({
+    url:
+    'http://api.giphy.com/v1/gifs/search?q=megatron&api_key=dc6zaTOxFJmzC&offset=' + page * 25
+  })
+  .done(function(apiResult) {
+    console.log(apiResult);
+    gifList.innerHTML ='';
+
+    for (var i = 0; i < apiResult.data.length; i++) {
+      var obj = apiResult.data[i];
+
+      var li = document.createElement('li');
+      var img = document.createElement('img');
+      img.src = obj.images.downsized.url;
+      li.appendChild(img);
+
+      gifList.appendChild(li);
+    }
+
+  });
+
+}
+loadData();
