5bfaa713d58321ad45b8c76a4e06c589284692f5
David Kurth <david-kurth@sbcglobal.net>
2016-12-16 17:43:07 -0600

    finished part one

--- week-7/day-34/day-34-github-repo-search/script.js
+++ week-7/day-34/day-34-github-repo-search/script.js
@@ -1,6 +1,7 @@
 (function(){
 
   var inputer =document.querySelector('.word-input');
+  var displayUL = document.querySelector('.display-list');
   var display =document.querySelector('.link-template');
 
   function requestData() {
@@ -10,9 +11,35 @@
 
     promise.done(function(data){
     console.log('this data', data);
-    // for (var i= 0; i < data.length; i++) {
-    //   var item = data[i];
-    // }
+
+    var templateHtml = display.innerHTML;
+
+
+    var html = '';
+    for (var i= 0; i < data.items.length; i++) {
+      var item = data.items[i];
+      console.log(item);
+      var output = Mustache.render(templateHtml, item);
+      html += output;
+    }
+      displayUL.innerHTML = html;
+
+
+
+    // var beerStuffList= document.querySelector('.beer-stuff');
+    //   var templateScript = document.querySelector('.beer-template');
+    //   var templateHtml = templateScript.innerHTML;
+    //
+    //   var html = '';
+    //   for (var i = 0; i < data.length; i++) {
+    //     var beer = data[i];
+    // console.log(beer);
+    //     var output = Mustache.render(templateHtml, beer);
+    //     html += output;
+    //   }
+    //   beerStuffList.innerHTML = html;
+
+
     });
   }
 
