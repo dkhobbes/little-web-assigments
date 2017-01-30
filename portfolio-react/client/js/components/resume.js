492a58874ce90550a1587c258fa1f5971d71d2b4
David Kurth <david-kurth@sbcglobal.net>
2017-01-30 14:49:52 -0600

    updating resume.js

---
+++ portfolio-react/client/js/components/resume.js
@@ -0,0 +1,16 @@
+window.Portfolio = window.Portfolio || {};
+(function() {
+
+  class ResumeComponent extends React.Component {
+    constructor(){
+     super();
+    }
+
+   render(){
+     return <div></div>
+    }
+  }
+
+Portfolio.ResumeComponent = ResumeComponent;
+
+})();
--- portfolio-react/public/components/resume.js
+++ portfolio-react/public/components/resume.js
@@ -1,2 +1,34 @@
 "use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+window.Portfolio = window.Portfolio || {};
+(function () {
+  var ResumeComponent = function (_React$Component) {
+    _inherits(ResumeComponent, _React$Component);
+
+    function ResumeComponent() {
+      _classCallCheck(this, ResumeComponent);
+
+      return _possibleConstructorReturn(this, (ResumeComponent.__proto__ || Object.getPrototypeOf(ResumeComponent)).call(this));
+    }
+
+    _createClass(ResumeComponent, [{
+      key: "render",
+      value: function render() {
+        return React.createElement("div", null);
+      }
+    }]);
+
+    return ResumeComponent;
+  }(React.Component);
+
+  Portfolio.ResumeComponent = ResumeComponent;
+})();
 //# sourceMappingURL=resume.js.map
